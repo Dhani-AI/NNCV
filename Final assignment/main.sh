da34607b0c3dd0e91fba773e939b2ca2f2c4d395
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-17 21:52:24 +0100

    Add LR scheduler

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,9 +3,11 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 20 \
-    --epochs 50 \
+    --epochs 65 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "dinov2-training" \
     --model "dinov2" \
+    --scheduler \
+    --scheduler-epochs 50 \
