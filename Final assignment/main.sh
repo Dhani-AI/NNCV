11aafa2a082918e1894e7156a3584bd8d582a848
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-18 11:51:11 +0100

    Change parameters

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,12 +2,12 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 4 \
-    --epochs 100 \
+    --batch-size 20 \
+    --epochs 65 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "dinov2-training" \
     --model "dinov2" \
     --scheduler \
-    --scheduler-epochs 30 60 90 \
+    --scheduler-epochs 50 \
