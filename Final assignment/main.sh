9687baa96d500814b390aea00559b52f07f9a562
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-19 21:14:19 +0100

    Change Batchsize

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 12 \
+    --batch-size 32 \
     --epochs 100 \
-    --lr 0.0005 \
+    --lr 0.005 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "dinov2-training" \
