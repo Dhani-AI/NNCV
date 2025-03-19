f34f3da0e3047fbd2ed5c57df9c0db4c0ffb6970
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-19 13:30:08 +0100

    Change Lr

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
     --epochs 65 \
-    --lr 0.0005 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "dinov2-training" \
