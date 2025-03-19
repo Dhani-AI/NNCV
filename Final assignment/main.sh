82e12a8c4296daeafa252679b8317417b14031a7
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-19 13:31:12 +0100

    Change epochs

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
-    --epochs 65 \
+    --epochs 100 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
