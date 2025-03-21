e5c245dd00c5ec80e2157a7ec9a04efac40ac3b9
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-21 17:59:04 +0100

    Change experiment id

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,7 +7,7 @@ python3 train.py \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov2-head-training" \
+    --experiment-id "dinov2-ade20k-backbone" \
     --model "dinov2" \
     --scheduler \
     --scheduler-epochs 80 \
