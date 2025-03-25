1e6418f04674c5a94b3d0c428b2613ae5fa4f0ca
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-25 16:16:58 +0100

    Remove env

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,9 +1,5 @@
 wandb login
 
-# Create a virtual environment in home directory (if not already created)
-export VENV_DIR="$HOME/5LSM0"
-source $VENV_DIR/bin/activate
-
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
