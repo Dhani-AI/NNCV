c57b5581ec792b8921df792e11248c20289a2a0a
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-03-25 15:21:09 +0100

    Add environment

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,5 +1,17 @@
 wandb login
 
+# Create a virtual environment in home directory (if not already created)
+export VENV_DIR="$HOME/venv_optuna"
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip install --upgrade pip
+    pip install xformers
+    pip install torchinfo
+else
+    source $VENV_DIR/bin/activate
+fi
+
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
