1afbb695bdac1d16a507d5089e1ecfdb6654757b
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-04-16 14:51:16 +0200

    Change hyperparameters

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,16 +3,16 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 32 \
-    --epochs 150 \
-    --lr 0.0001 \
+    --epochs 100 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov2-finetune-small-32" \
+    --experiment-id "dinov2-pretrained-small-32" \
     --model "dinov2" \
     --weight-decay 0.0001 \
     --multistep \
-    --scheduler-epochs 80 \
-    --fine-tune \
+    --scheduler-epochs 40 60 80 \
+    # --fine-tune \
     # --onecycle \
     # --weighted \
     
