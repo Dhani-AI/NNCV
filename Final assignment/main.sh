93bf07d6f37a1695460d869e315681a76104ce7a
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-04-18 16:54:40 +0200

    baseline parameters

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,12 +2,12 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 32 \
+    --batch-size 8 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov2-pretrained-small-32" \
+    --experiment-id "dinov2-small-baseline" \
     --model "dinov2" \
     --weight-decay 0.0001 \
     --multistep \
