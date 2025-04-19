12f28affe39f2e846bda5382b6f50bd5b2378388
Dhani-AI <d.r.m.crapels@student.tue.nl>
2025-04-19 09:15:38 +0200

    change

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,17 +2,17 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 8 \
+    --batch-size 64 \
     --epochs 100 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov2-small-ASPP" \
+    --experiment-id "dinov2-small-FPN" \
     --model "dinov2" \
     --weight-decay 0.0001 \
-    --multistep \
-    --scheduler-epochs 40 60 80 \
+    --onecycle \
+    # --multistep \
+    # --scheduler-epochs 40 60 80 \
     # --fine-tune \
-    # --onecycle \
     # --weighted \
     
